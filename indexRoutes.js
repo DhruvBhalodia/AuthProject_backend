e01c393b4875a3b25c0bff456e194c994e5d0400
DhruvBhalodia <dhruvcoder007@gmail.com>
2025-06-12 11:47:23 +0530

    Revert "Merge pull request #2 from DhruvBhalodia/develop"
    This reverts commit 1ab395b13dd99e13657678b26ebf1a34578fbf9f, reversing
    changes made to 9d55abfb3d25d2caf8c2e19aa43e6370074fdc4c.

--- indexRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const authRoutes = require('./components/auth/auth.route');
-
-router.use('/auth', authRoutes);
-
-module.exports = router;
